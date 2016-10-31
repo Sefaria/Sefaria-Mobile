2b6c391e8cf8697a856754cc9282b402bcb25c9f
Russel Neiss <russel.neiss@gmail.com>
2016-10-31 12:51:21 -0500

    add on layout event for initY on segmented

--- ReaderApp/TextColumn.js
+++ ReaderApp/TextColumn.js
@@ -664,7 +664,15 @@ var TextColumn = React.createClass({
 
     //console.log("Rendering Row:", reactRef);
 
-    return <View style={styles.verseContainer} key={reactRef} ref={(view)=>this.rowRefs[reactRef]=view}>{segment}</View>;
+    return <View style={styles.verseContainer} key={reactRef} ref={(view)=>this.rowRefs[reactRef]=view}
+
+                                             onLayout={(event) => {
+                                       var {x, y, width, height} = event.nativeEvent.layout;
+                                       this.rowRefs[reactRef]._initY = y;
+                                       }
+                                     }
+
+    >{segment}</View>;
   },
   rowHasChanged: function(r1, r2) {
     // console.log(r1.changeString + " vs. " + r2.changeString);
