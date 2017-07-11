b13f054b255649e7e844096e1268c1635dbd3661
Russel Neiss <russel.neiss@gmail.com>
2017-07-11 00:21:49 -0400

    RN 0.42.0

--- ReaderApp/android/app/src/main/java/com/readerapp/MainApplication.java
+++ ReaderApp/android/app/src/main/java/com/readerapp/MainApplication.java
@@ -1,7 +1,6 @@
 package com.readerapp;
 
 import android.app.Application;
-import android.util.Log;
 
 import com.facebook.react.ReactApplication;
 import com.rnziparchive.RNZipArchivePackage;
@@ -18,7 +17,7 @@ public class MainApplication extends Application implements ReactApplication {
 
   private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
     @Override
-    protected boolean getUseDeveloperSupport() {
+    public boolean getUseDeveloperSupport() {
       return BuildConfig.DEBUG;
     }
 
