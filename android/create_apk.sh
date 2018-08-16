8d855a303cf6d8886aa2ca4c5d5e990da05c58f7
Noah Santacruz <noahssantacruz@gmail.com>
2018-08-16 12:16:56 +0300

    updating build.gradle to 27. improving create_apk script

--- android/create_apk.sh
+++ android/create_apk.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
+. ./keystore_pass.sh  # include KEYSTORE_PASS variable
 KEYSTORE="kstore.jks"
 APK_DIR="app/build/outputs/apk/release"
 curl "http://localhost:8081/index.android.bundle?platform=android" -o "app/src/main/assets/index.android.bundle"
 ./gradlew assembleRelease
-jarsigner -verbose -keystore $KSTORE $APK_DIR/app-release-unsigned.apk kstore
-zipalign -f -v 4 $APK_DIR/app-release-unsigned.apk $APK_DIR/app-release-signed.apk 
+jarsigner -keystore $KEYSTORE -storepass $KEYSTORE_PASS $APK_DIR/app-release-unsigned.apk kstore
+zipalign -f 4 $APK_DIR/app-release-unsigned.apk $APK_DIR/app-release-signed.apk
---
+++ android/keystore_pass_example.sh
@@ -0,0 +1,3 @@
+#!/usr/bin/env bash
+# make a copy of this file called keystore_pass.sh and fill in the password below, assuming you know it ;)
+KEYSTORE_PASS=""
