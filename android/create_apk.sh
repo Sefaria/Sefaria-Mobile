ee98ffd99d98f5a33b7cd4fa2a1d1171032edfff
Noah Santacruz <noahssantacruz@gmail.com>
2018-08-16 14:56:14 +0300

    removing unused android permissions from react native

--- android/create_apk.sh
+++ android/create_apk.sh
@@ -4,5 +4,6 @@ KEYSTORE="kstore.jks"
 APK_DIR="app/build/outputs/apk/release"
 curl "http://localhost:8081/index.android.bundle?platform=android" -o "app/src/main/assets/index.android.bundle"
 ./gradlew assembleRelease
-jarsigner -keystore $KEYSTORE -storepass $KEYSTORE_PASS $APK_DIR/app-release-unsigned.apk kstore
+jarsigner -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE -storepass $KEYSTORE_PASS $APK_DIR/app-release-unsigned.apk kstore
+jarsigner -verify $APK_DIR/app-release-unsigned.apk
 zipalign -f 4 $APK_DIR/app-release-unsigned.apk $APK_DIR/app-release-signed.apk
