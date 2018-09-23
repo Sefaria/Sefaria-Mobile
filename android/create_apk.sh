618563e2bfcb14107951ab188cd31bef6d26f7c7
Noah Santacruz <noahssantacruz@gmail.com>
2018-09-23 11:09:54 +0300

    add some docs to create_apk.sh

--- android/create_apk.sh
+++ android/create_apk.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+# before you run, up version in app/build.gradle
+
 . ./keystore_pass.sh  # include KEYSTORE_PASS variable
 KEYSTORE="kstore.jks"
 APK_DIR="app/build/outputs/apk/release"
@@ -7,3 +9,5 @@ curl "http://localhost:8081/index.android.bundle?platform=android" -o "app/src/m
 jarsigner -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE -storepass $KEYSTORE_PASS $APK_DIR/app-release-unsigned.apk kstore
 jarsigner -verify $APK_DIR/app-release-unsigned.apk
 zipalign -f 4 $APK_DIR/app-release-unsigned.apk $APK_DIR/app-release-signed.apk
+
+# after its signed, go to https://play.google.com/apps/publish/?account=8012316618525581861#ManageReleasesPlace:p=org.sefaria.sefaria
