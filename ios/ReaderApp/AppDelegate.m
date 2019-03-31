5b0d919695a41977f11837e396e6414d1a76e767
Noah Santacruz <noahssantacruz@gmail.com>
2019-03-31 14:52:53 +0300

    0.57.8

--- ios/ReaderApp/AppDelegate.m
+++ ios/ReaderApp/AppDelegate.m
@@ -18,8 +18,11 @@
   [NSThread sleepForTimeInterval:1.4];
   NSURL *jsCodeLocation;
 
-  jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
-
+  #ifdef DEBUG
+    jsCodeLocation = [[RCTBundleURLProvider sharedSettings] jsBundleURLForBundleRoot:@"index" fallbackResource:nil];
+  #else
+    jsCodeLocation = [[NSBundle mainBundle] URLForResource:@"main" withExtension:@"jsbundle"];
+  #endif
   RCTRootView *rootView = [[RCTRootView alloc] initWithBundleURL:jsCodeLocation
                                                       moduleName:@"ReaderApp"
                                                initialProperties:nil
