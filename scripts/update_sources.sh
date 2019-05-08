db9d9ccd51f29e2aa2260b4599a41e7dca7b825d
Noah Santacruz <noahssantacruz@gmail.com>
2019-05-08 16:58:14 +0300

    5.2.3

--- scripts/update_sources.sh
+++ scripts/update_sources.sh
@@ -5,12 +5,13 @@ cd "${0%/*}"  # cd to dir of this script. see https://stackoverflow.com/question
 echo "Updating IOS and Android source files from readonly environment on old dev server"
 IOS_SOURCES="../ios/sources/"
 ANDROID_SOURCES="../android/app/src/main/assets/sources/"
-scp sefaria-dev:/var/www/readonly/static/ios-export/4/packages.json $IOS_SOURCES
-scp sefaria-dev:/var/www/readonly/static/ios-export/4/people.json $IOS_SOURCES
-scp sefaria-dev:/var/www/readonly/static/ios-export/4/toc.json $IOS_SOURCES
-scp sefaria-dev:/var/www/readonly/static/ios-export/4/search_toc.json $IOS_SOURCES
-scp sefaria-dev:/var/www/readonly/static/ios-export/4/hebrew_categories.json $IOS_SOURCES
-scp sefaria-dev:/var/www/readonly/static/ios-export/4/calendar.json $IOS_SOURCES
+EXPORT_VERSION="5"
+scp sefaria-dev:/var/www/readonly/static/ios-export/$EXPORT_VERSION/packages.json $IOS_SOURCES
+scp sefaria-dev:/var/www/readonly/static/ios-export/$EXPORT_VERSION/people.json $IOS_SOURCES
+scp sefaria-dev:/var/www/readonly/static/ios-export/$EXPORT_VERSION/toc.json $IOS_SOURCES
+scp sefaria-dev:/var/www/readonly/static/ios-export/$EXPORT_VERSION/search_toc.json $IOS_SOURCES
+scp sefaria-dev:/var/www/readonly/static/ios-export/$EXPORT_VERSION/hebrew_categories.json $IOS_SOURCES
+scp sefaria-dev:/var/www/readonly/static/ios-export/$EXPORT_VERSION/calendar.json $IOS_SOURCES
 cp $IOS_SOURCES/packages.json $ANDROID_SOURCES
 cp $IOS_SOURCES/people.json $ANDROID_SOURCES
 cp $IOS_SOURCES/toc.json $ANDROID_SOURCES
