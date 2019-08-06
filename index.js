dc4949dff16a96e8b5e6e5a830779e3cfcc2b70c
Noah Santacruz <noahssantacruz@gmail.com>
2019-08-06 16:54:07 +0300

    migrate to context

--- index.js
+++ index.js
@@ -7,14 +7,27 @@ import {
   View,
 } from 'react-native';
 import React from 'react';
-import { Provider } from 'react-redux';
-import { store } from './ReduxStore';
+import {
+  GlobalStateContext,
+  DispatchContext,
+  reducer,
+  DEFAULT_STATE,
+} from './StateManager';
 import ReaderApp from './ReaderApp';
 
-const Root = () => (
-  <Provider store={store}>
-    <ReaderApp />
-  </Provider>
-);
+const Root = () => {
+  const [ globalState, dispatch ] = React.useReducer(reducer, DEFAULT_STATE);
+  // ReaderApp props are for use in componentWillUpdate()
+  return (
+    <DispatchContext.Provider value={dispatch}>
+      <GlobalStateContext.Provider value={globalState}>
+        <ReaderApp
+          textLanguage={globalState.textLanguage}
+          themeStr={globalState.themeStr}
+        />
+      </GlobalStateContext.Provider>
+    </DispatchContext.Provider>
+  );
+};
 
-AppRegistry.registerComponent('ReaderApp', () => Root);
+AppRegistry.registerComponent('ReaderApp', Root);
