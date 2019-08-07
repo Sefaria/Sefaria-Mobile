05a60727a67200000e55d683c8641535a377b601
Noah Santacruz <noahssantacruz@gmail.com>
2019-08-07 17:41:27 +0300

    migrate TextSegment to hooks

--- TextSegment.js
+++ TextSegment.js
@@ -2,7 +2,7 @@
 
 import PropTypes from 'prop-types';
 
-import React from 'react';
+import React, { useState, useEffect, useContext } from 'react';
 import {
   Text,
   TouchableOpacity,
@@ -10,105 +10,94 @@ import {
   Platform,
 } from 'react-native';
 import HTMLView from 'react-native-htmlview'; //to convert html'afied JSON to something react can render (https://github.com/jsdf/react-native-htmlview)
+import { GlobalStateContext } from './StateManager';
 import styles from './Styles.js';
 import iPad from './isIPad';
 
 
-class TextSegment extends React.PureComponent {
-  static propTypes = {
-    theme:              PropTypes.object.isRequired,
-    themeStr:           PropTypes.string.isRequired,
-    rowRef:             PropTypes.string.isRequired, /* this ref keys into TextColumn.rowRefs */
-    segmentKey:         PropTypes.string,
-    data:               PropTypes.string,
-    textType:           PropTypes.oneOf(["english","hebrew"]),
-    bilingual:          PropTypes.bool,
-    biLayout:           PropTypes.oneOf(["stacked", "sidebyside", "sidebysiderev"]),
-    textSegmentPressed: PropTypes.func.isRequired,
-    onLongPress:        PropTypes.func.isRequired,
-    fontSize:           PropTypes.number.isRequired,
-  };
+const TextSegment = ({
+  rowRef,
+  segmentKey,
+  data,
+  textType,
+  bilingual,
+  textSegmentPressed,
+  onLongPress,
+}) => {
+  const [resetKey, setResetKey] = useState(0);
+  const { themeStr, fontSize, biLayout } = useContext(GlobalStateContext);
+  useEffect(() => {
+    setResetKey(resetKey+1);  // hacky fix to reset htmlview when theme colors change
+    return () => {};
+  }, [themeStr, fontSize]);
 
-  constructor(props) {
-    super(props);
-    this.state = {
-      resetKey: 0
-    };
-  }
-  onPressTextSegment = () => {
-    let key = this.props.segmentKey;
+  const onPress = () => {
+    let key = segmentKey;
     let section = parseInt(key.split(":")[0]);
     let segment = parseInt(key.split(":")[1]);
-    this.props.textSegmentPressed(section, segment, this.props.rowRef, true);
+    textSegmentPressed(section, segment, rowRef, true);
   };
 
-  componentWillReceiveProps(nextProps) {
-    if (this.props.themeStr !== nextProps.themeStr ||
-        this.props.fontSize !== nextProps.fontSize) {
-      this.setState({ resetKey: !this.state.resetKey }); //hacky fix to reset htmlview when theme colors change
+  const isStacked = biLayout === 'stacked';
+  const lineHeightMultiplierHe = Platform.OS === 'android' ? 1.3 : 1.2;
+  const justifyStyle = {textAlign: (isStacked && Platform.OS === 'ios') ? 'justify' : (textType === 'hebrew' ? 'right' : 'left')};
+  const style = textType == "hebrew" ?
+                [styles.hebrewText, theme.text, justifyStyle, {fontSize, lineHeight: fontSize * lineHeightMultiplierHe}] :
+                [styles.englishText, theme.text, justifyStyle, {fontSize: 0.8 * fontSize, lineHeight: fontSize * 1.04 }];
+  if (bilingual && textType == "english") {
+    if (isStacked) {
+      style.push(styles.bilingualEnglishText);
     }
+    style.push(theme.bilingualEnglishText);
   }
-  render() {
-    // console.log(this.props.segmentKey+": "+typeof(this.props.textRef));
-    const isStacked = this.props.biLayout === 'stacked';
-    const lineHeightMultiplierHe = Platform.OS === 'android' ? 1.3 : 1.2;
-    const justifyStyle = {textAlign: (isStacked && Platform.OS === 'ios') ? 'justify' : (this.props.textType === 'hebrew' ? 'right' : 'left')};
-    const style = this.props.textType == "hebrew" ?
-                  [styles.hebrewText, this.props.theme.text, justifyStyle, {fontSize: this.props.fontSize, lineHeight: this.props.fontSize * lineHeightMultiplierHe}] :
-                  [styles.englishText, this.props.theme.text, justifyStyle, {fontSize: 0.8 * this.props.fontSize, lineHeight: this.props.fontSize * 1.04 }];
-    if (this.props.bilingual && this.props.textType == "english") {
-      if (isStacked) {
-        style.push(styles.bilingualEnglishText);
-      }
-      style.push(this.props.theme.bilingualEnglishText);
+  const smallSheet = {
+    small: {
+      fontSize: fontSize * 0.8 * (textType === "hebrew" ? 1 : 0.8)
+    },
+    hediv: {
+      ...styles.hediv,
+      ...justifyStyle,
+    },
+    endiv: {
+      ...styles.endiv,
+      ...justifyStyle,
     }
-    const smallSheet = {
-      small: {
-        fontSize: this.props.fontSize * 0.8 * (this.props.textType === "hebrew" ? 1 : 0.8)
-      },
-      hediv: {
-        ...styles.hediv,
-        ...justifyStyle,
-      },
-      endiv: {
-        ...styles.endiv,
-        ...justifyStyle,
-      }
-    };
-    // return (
-    //   <TouchableOpacity style={{flex:1}} onPress={this.onPressTextSegment}>
-    //     <Text suppressHighlighting={false} key={this.props.segmentKey} style={style}>
-    //       { data }
-    //     </Text>
-    //   </TouchableOpacity>
-    // )
-    return (
-           <HTMLView
-             key={this.state.resetKey}
-             value={this.props.data}
-             stylesheet={{...styles, ...smallSheet}}
-             rootComponentProps={{
-                 hitSlop: {top: 10, bottom: 10, left: 10, right: 10},  // increase hit area of segments
-                 onPress:this.onPressTextSegment,
-                 onLongPress:this.props.onLongPress,
-                 delayPressIn: 200,
-               }
-             }
-             RootComponent={TouchableOpacity}
-             textComponentProps={
-               {
-                 suppressHighlighting: false,
-                 key:this.props.segmentKey,
-                 style: style,
+  };
+  return (
+     <HTMLView
+       key={resetKey}
+       value={data}
+       stylesheet={{...styles, ...smallSheet}}
+       rootComponentProps={{
+           hitSlop: {top: 10, bottom: 10, left: 10, right: 10},  // increase hit area of segments
+           onPress,
+           onLongPress,
+           delayPressIn: 200,
+         }
+       }
+       RootComponent={TouchableOpacity}
+       textComponentProps={
+         {
+           suppressHighlighting: false,
+           key: segmentKey,
+           style: style,
 
-               }
-             }
-             style={{flex: this.props.textType == "hebrew" ? 4.5 : 5.5, paddingHorizontal: 10}}
-           />
+         }
+       }
+       style={{flex: textType == "hebrew" ? 4.5 : 5.5, paddingHorizontal: 10}}
+     />
 
-    );
-  }
+  );
 }
+TextSegment.propTypes = {
+  rowRef:             PropTypes.string.isRequired, /* this ref keys into TextColumn.rowRefs */
+  segmentKey:         PropTypes.string,
+  data:               PropTypes.string,
+  textType:           PropTypes.oneOf(["english","hebrew"]),
+  bilingual:          PropTypes.bool,
+  textSegmentPressed: PropTypes.func.isRequired,
+  onLongPress:        PropTypes.func.isRequired,
+};
 
 
 export default TextSegment;
