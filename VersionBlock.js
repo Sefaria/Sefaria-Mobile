2beba6da82dd2007f23897a49635e80e713008be
Noah Santacruz <noahssantacruz@gmail.com>
2019-08-08 11:40:18 +0300

    migrate VersionBlock to hooks

--- VersionBlock.js
+++ VersionBlock.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext, useCallback } from 'react';
 import PropTypes from 'prop-types';
 import {
   View,
@@ -8,99 +8,103 @@ import {
 import {
   SText,
 } from './Misc';
+import { GlobalStateContext } from './StateManager';
 
 import HTMLView from 'react-native-htmlview'; //to convert html'afied JSON to something react can render (https://github.com/jsdf/react-native-htmlview)
 import styles from './Styles.js';
 
 
-class VersionBlock extends React.Component {
-  static propTypes = {
-    theme:                PropTypes.object.isRequired,
-    version:              PropTypes.object.isRequired,
-    openVersionInSidebar: PropTypes.func,
-    openVersionInReader:  PropTypes.func,
-    isCurrent:            PropTypes.bool,
-    interfaceLang:        PropTypes.oneOf(["english", "hebrew"]),
-    openUri:              PropTypes.func.isRequired,
-  };
+const VersionBlock = ({
+  version,
+  openVersionInSidebar,
+  openVersionInReader,
+  isCurrent,
+  openUri,
+}) => {
+  const { theme, textLanguage } = useContext(GlobalStateContext);
 
-  onVersionTitleClick = () => {
-    const action = this.props.openVersionInSidebar ? this.props.openVersionInSidebar : this.props.openVersionInReader;
+  const onVersionTitleClick = useCallback(() => {
+    const action = openVersionInSidebar ? openVersionInSidebar : openVersionInReader;
     if (action) {
-      action(this.props.version.versionTitle, this.props.version.versionTitleInHebrew, this.props.version.language);
+      action(version.versionTitle, version.versionTitleInHebrew, version.language);
     }
-  }
-  onSelectVersionClick = () => {
-    if (this.props.openVersionInReader) {
-      this.props.openVersionInReader(this.props.version.versionTitle, this.props.version.language);
+  }, [version]);
+  const onSelectVersionClick = useCallback(() => {
+    if (openVersionInReader) {
+      openVersionInReader(version.versionTitle, version.language);
     }
-  }
-  render() {
-    const versionInfo = this.props.version;
-    let versionTitle, versionSource, shortVersionSource, license, licenseURL, versionNotes;
-    if (this.props.textLang == "hebrew") {
-      versionTitle = versionInfo['heVersionTitle'];
-      versionSource = versionInfo['heVersionSource'];
-      shortVersionSource = Sefaria.util.parseURLhost(versionSource);
-      license = versionInfo['heLicense'];
-      licenseURL = Sefaria.util.getLicenseURL(license);
-      versionNotes = versionInfo['heVersionNotes'];
-    } else {
-      versionTitle = versionInfo['versionTitle'];
-      versionSource = versionInfo['versionSource'];
-      shortVersionSource = Sefaria.util.parseURLhost(versionSource);
-      license = versionInfo['license'];
-      licenseURL = Sefaria.util.getLicenseURL(license);
-      versionNotes = versionInfo['versionNotes'];
-    }
+  }, [version]);
 
-    const textAlign = { textAlign: "left" };
-    if (license === "CC-BY") { console.log(versionSource, license)}
-    return (
-      <View>
-        {
-          versionTitle ?
-            (this.props.openVersionInSidebar ?
-              <TouchableOpacity onPress={this.onVersionTitleClick}>
-                <Text style={[styles.en, styles.textTocVersionTitle, textAlign, this.props.theme.text]}>
-                  {versionTitle}
-                </Text>
-              </TouchableOpacity> :
-              <SText lang={"english"} style={[styles.en, styles.textTocVersionTitle, textAlign, this.props.theme.text]}>
+  let versionTitle, versionSource, shortVersionSource, license, licenseURL, versionNotes;
+  if (textLanguage == "hebrew") {
+    versionTitle = version['heVersionTitle'];
+    versionSource = version['heVersionSource'];
+    shortVersionSource = Sefaria.util.parseURLhost(versionSource);
+    license = version['heLicense'];
+    licenseURL = Sefaria.util.getLicenseURL(license);
+    versionNotes = version['heVersionNotes'];
+  } else {
+    versionTitle = version['versionTitle'];
+    versionSource = version['versionSource'];
+    shortVersionSource = Sefaria.util.parseURLhost(versionSource);
+    license = version['license'];
+    licenseURL = Sefaria.util.getLicenseURL(license);
+    versionNotes = version['versionNotes'];
+  }
+
+  const textAlign = { textAlign: "left" };
+  if (license === "CC-BY") { console.log(versionSource, license)}
+  return (
+    <View>
+      {
+        versionTitle ?
+          (this.props.openVersionInSidebar ?
+            <TouchableOpacity onPress={onVersionTitleClick}>
+              <Text style={[styles.en, styles.textTocVersionTitle, textAlign, theme.text]}>
                 {versionTitle}
-              </SText>)
+              </Text>
+            </TouchableOpacity> :
+            <SText lang={"english"} style={[styles.en, styles.textTocVersionTitle, textAlign, theme.text]}>
+              {versionTitle}
+            </SText>)
+        : null
+      }
+      <View style={styles.textTocVersionInfo}>
+        { versionSource ?
+          <TouchableOpacity onPress={() => { openUri(versionSource); }}>
+            <Text style={[styles.textTocVersionInfoText, theme.tertiaryText]}>{shortVersionSource || versionSource}</Text>
+          </TouchableOpacity>
           : null
         }
-        <View style={styles.textTocVersionInfo}>
-          { versionSource ?
-            <TouchableOpacity onPress={() => {this.props.openUri(versionSource);}}>
-              <Text style={[styles.textTocVersionInfoText, this.props.theme.tertiaryText]}>{shortVersionSource || versionSource}</Text>
-            </TouchableOpacity>
-            : null
-          }
-          { versionSource && (license && license !== "unknown") ?
-            <Text style={[styles.navBottomLinkDot, this.props.theme.tertiaryText]}>•</Text>
-            : null
-          }
-          { license && license !== "unknown" ?
-            <TouchableOpacity onPress={() => licenseURL ? this.props.openUri(licenseURL) : null}>
-              <Text style={[styles.textTocVersionInfoText, this.props.theme.tertiaryText]}>{license}</Text>
-            </TouchableOpacity>
-            : null
-          }
-        </View>
-        { versionNotes ?
-          <HTMLView
-            value={"<div>"+versionInfo['versionNotes']+"</div>"}
-            onLinkPress={(url) => this.props.openUri(url) }
-            stylesheet={styles}
-            textComponentProps={{style: [styles.textTocVersionNotes, textAlign, this.props.theme.tertiaryText]}}
-          />
+        { versionSource && (license && license !== "unknown") ?
+          <Text style={[styles.navBottomLinkDot, theme.tertiaryText]}>•</Text>
+          : null
+        }
+        { license && license !== "unknown" ?
+          <TouchableOpacity onPress={() => licenseURL ? openUri(licenseURL) : null}>
+            <Text style={[styles.textTocVersionInfoText, theme.tertiaryText]}>{license}</Text>
+          </TouchableOpacity>
           : null
         }
       </View>
-    );
-  }
+      { versionNotes ?
+        <HTMLView
+          value={"<div>"+version['versionNotes']+"</div>"}
+          onLinkPress={(url) => openUri(url) }
+          stylesheet={styles}
+          textComponentProps={{style: [styles.textTocVersionNotes, textAlign, theme.tertiaryText]}}
+        />
+        : null
+      }
+    </View>
+  );
 }
+VersionBlock.propTypes = {
+  version:              PropTypes.object.isRequired,
+  openVersionInSidebar: PropTypes.func,
+  openVersionInReader:  PropTypes.func,
+  isCurrent:            PropTypes.bool,
+  openUri:              PropTypes.func.isRequired,
+};
 
 export default VersionBlock;
