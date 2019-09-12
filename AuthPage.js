16153f93307b9b9460fa4bc906702816d33b7fec
Noah Santacruz <noahssantacruz@gmail.com>
2019-09-12 14:23:46 +0300

    hebrew styles for authpage

--- AuthPage.js
+++ AuthPage.js
@@ -94,7 +94,7 @@ const AuthPage = ({ authMode, close, showToast, openLogin, openRegister, openUri
   const placeholderTextColor = themeStr == "black" ? "#BBB" : "#777";
   const isHeb = interfaceLanguage === 'hebrew';
   return(
-    <ScrollView style={{flex:1, alignSelf: "stretch" }} contentContainerStyle={{alignItems: "center"}}>
+    <ScrollView style={{flex:1, alignSelf: "stretch"}} contentContainerStyle={{alignItems: "center", paddingBottom: 50}}>
       <RainbowBar />
       <View style={{ flex: 1, alignSelf: "stretch", alignItems: "flex-end", marginHorizontal: 10}}>
         <CircleCloseButton onPress={close} />
@@ -257,7 +257,7 @@ const LogInMotivator = ({
   }
   return (
     <View style={{
-        flexDirection: 'row',
+        flexDirection: isHeb ? 'row-reverse' : 'row',
         marginBottom: 20,
       }}
     >
