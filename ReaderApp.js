f23deb2f11b740643b5ce81481eb6031b43dde78
Noah Santacruz <noahssantacruz@gmail.com>
2019-10-03 15:40:15 +0300

    remove console log

--- ReaderApp.js
+++ ReaderApp.js
@@ -595,8 +595,6 @@ class ReaderApp extends React.PureComponent {
     });
   }
 
-  isHermes = () => global.HermesInternal != null;
-
   removeDefaultVersions = (ref, versions) => {
     if (!versions) return versions;
     const cachedVersionList = Sefaria.api.getCachedVersions(ref);
@@ -1936,7 +1934,6 @@ class ReaderApp extends React.PureComponent {
   }
 
   render() {
-    console.log("HERMES", this.isHermes());
     /*
     // make the SafeAreaView background based on the category color
     const cat = this.state.menuOpen ? (this.state.navigationCategories.length ? this.state.navigationCategories[0] : "Other") : Sefaria.categoryForTitle(this.state.textTitle);
