2d4f0fcf28a649da9fbfee5237f2b9debbcdcf4e
Jonathan <jonathan@sefaria.org>
2019-11-25 15:08:30 +0200

    airplane mode settings crash bugfix

--- __tests__/ReaderApp.test.js
+++ __tests__/ReaderApp.test.js
@@ -33,3 +33,35 @@ describe('ReaderApp history', () => {
     expect(BackgroundFetch.finish).toBeCalledWith(BackgroundFetch.FETCH_RESULT_NEW_DATA);
   });
 });
+
+describe('ReaderApp noInternet', () =>{
+  // todo: finish this test (xtest skips the test)
+  xtest('loadNewText', async () => {
+    // fetch = jest.fn(() => Promise.reject(e => null));
+    fetch = jest.fn(() => Promise.resolve({
+      status: 200,
+      json() {
+        return Promise.resolve({
+          user_history: webHistory,
+          last_sync: 11,
+          settings: {
+            time_stamp: 11,
+          },
+        })
+      }
+    }));
+    const inst = renderer.create(<TestContextWrapper passContextToChildren child={ReaderApp} />);
+    const readerApp = inst.root.findByType(ReaderApp);
+    const resolve = jest.fn();
+    readerApp.instance.setState = jest.fn();
+    readerApp.props.dispatch = jest.fn();
+    const loadNewText = () => {
+      return readerApp.instance.loadNewText({ref: 'Genesis 1', versions: null, overwriteVersions: false});
+    }
+    // await act(loadNewText);
+    // return readerApp.loadNewText({ref: 'Genesis 1', versions: null, overwriteVersions: false})
+    // .then(data => expect(data).toBe('foo'));
+    await loadNewText().then(resolve);
+    expect(resolve.mock.calls.length).toBe(1);
+  })
+});
