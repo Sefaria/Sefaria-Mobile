c8acc9ddc7e27579718273a6b17ecf246bbe193c
Noah Santacruz <noahssantacruz@gmail.com>
2020-04-01 12:23:02 +0300

    remove selectabletextview module

--- android/app/src/main/java/com/readerapp/selectabletextview/SelectableTextViewPackage.java
+++ android/app/src/main/java/com/readerapp/selectabletextview/SelectableTextViewPackage.java
@@ -5,15 +5,14 @@ import com.facebook.react.bridge.NativeModule;
 import com.facebook.react.bridge.ReactApplicationContext;
 import com.facebook.react.uimanager.ViewManager;
 
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 public class SelectableTextViewPackage implements ReactPackage {
     @Override
     public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(
-          new SelectableTextViewModule(reactContext)
-      );
+      return new ArrayList<>();
     }
 
     
