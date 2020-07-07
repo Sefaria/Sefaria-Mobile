30bf0fcbb197bc6271680c7ade065cda761ed79d
Noah <noahssantacruz@gmail.com>
2020-07-07 13:00:46 +0300

    remove touchableopacity to debug selection

--- TextSegment.js
+++ TextSegment.js
@@ -88,12 +88,6 @@ const TextSegment = React.memo(({
     menuItems.splice(1, 1);
   }
   return (
-    <TouchableOpacity
-      hitSlop= {{top: 10, bottom: 10, left: 10, right: 10}}
-      onPress={() => onPress()}
-      onLongPress={() => {}}
-      delayPressIn={200}
-    >
       <SelectableText
         menuItems={menuItems}
         onSelection={({ eventType, content }) => {
@@ -116,7 +110,6 @@ const TextSegment = React.memo(({
           },
         }}
       />
-    </TouchableOpacity>
   );
 });
 TextSegment.whyDidYouRender = true;
