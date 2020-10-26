7ab8caa8c9be20c7987766f45f279cfd9e52b2f6
Noah Santacruz <noahssantacruz@gmail.com>
2020-10-26 15:42:17 +0200

    add elucidating comment

--- Hooks.js
+++ Hooks.js
@@ -2,6 +2,10 @@
 import React, { useState, useEffect, useMemo, useCallback, useRef } from 'react';
 
 export const useAsyncVariable = (initIsLoaded, loadVariable, onLoad) => {
+  /*
+  Loads a variable asynchronously and returns status of load
+  Useful for determining when a variable from the Sefaria object is available, e.g. Sefaria.calendar
+  */
   const [isLoaded, setIsLoaded] = useState(initIsLoaded);
   const setup = async (isLoaded) => {
     if (!isLoaded) {
