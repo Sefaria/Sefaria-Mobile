1ffc8fd62841a8c8eee9c603350e6f0f54e5f6e8
nsantacruz <noahssantacruz@gmail.com>
2022-02-17 15:56:52 +0200

    ios working!

--- ios/yo.swift
+++ ios/yo.swift
@@ -1,9 +1,9 @@
-	//
+//
 //  yo.swift
 //  ReaderApp
 //
-//  Created by Noah Santacruz on 9/24/19.
-//  Copyright © 2019 Facebook. All rights reserved.
+//  Created by nss on 17/02/2022.
+//  Copyright © 2022 Facebook. All rights reserved.
 //
 
 import Foundation
