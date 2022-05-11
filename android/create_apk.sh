301c6f13844e478f1413f415ac741a56a4793941
Russel Neiss <russel.neiss@gmail.com>
2022-05-11 13:56:48 -0500

    update create_apk.sh to work on SDK 30+

--- android/create_apk.sh
+++ android/create_apk.sh
@@ -8,22 +8,24 @@ curl "http://localhost:8081/index.android.bundle?platform=android" -o "app/src/m
 ./gradlew assembleRelease
 
 # arm64-v8a
-jarsigner -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE -storepass $KEYSTORE_PASS $APK_DIR/app-arm64-v8a-release-unsigned.apk kstore
-jarsigner -verify $APK_DIR/app-arm64-v8a-release-unsigned.apk
 zipalign -f 4 $APK_DIR/app-arm64-v8a-release-unsigned.apk $APK_DIR/app-arm64-v8a-release-signed.apk
+apksigner sign --ks $KEYSTORE --ks-pass pass:$KEYSTORE_PASS --v1-signing-enabled true --v2-signing-enabled true $APK_DIR/app-arm64-v8a-release-signed.apk
+
 
 # armeabi-v7a
-jarsigner -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE -storepass $KEYSTORE_PASS $APK_DIR/app-armeabi-v7a-release-unsigned.apk kstore
-jarsigner -verify $APK_DIR/app-armeabi-v7a-release-unsigned.apk
 zipalign -f 4 $APK_DIR/app-armeabi-v7a-release-unsigned.apk $APK_DIR/app-armeabi-v7a-release-signed.apk
+apksigner sign --ks $KEYSTORE --ks-pass pass:$KEYSTORE_PASS --v1-signing-enabled true --v2-signing-enabled true $APK_DIR/app-armeabi-v7a-release-signed.apk
+
+
 
 # x86_64
-jarsigner -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE -storepass $KEYSTORE_PASS $APK_DIR/app-x86_64-release-unsigned.apk kstore
-jarsigner -verify $APK_DIR/app-x86_64-release-unsigned.apk
 zipalign -f 4 $APK_DIR/app-x86_64-release-unsigned.apk $APK_DIR/app-x86_64-release-signed.apk
+apksigner sign --ks $KEYSTORE --ks-pass pass:$KEYSTORE_PASS --v1-signing-enabled true --v2-signing-enabled true $APK_DIR/app-x86_64-release-signed.apk
+
 
 # x86
-jarsigner -tsa http://timestamp.comodoca.com/rfc3161 -sigalg SHA1withRSA -digestalg SHA1 -keystore $KEYSTORE -storepass $KEYSTORE_PASS $APK_DIR/app-x86-release-unsigned.apk kstore
-jarsigner -verify $APK_DIR/app-x86-release-unsigned.apk
 zipalign -f 4 $APK_DIR/app-x86-release-unsigned.apk $APK_DIR/app-x86-release-signed.apk
+apksigner sign --ks $KEYSTORE --ks-pass pass:$KEYSTORE_PASS --v1-signing-enabled true --v2-signing-enabled true $APK_DIR/app-x86-release-signed.apk
+
+
 # after its signed, go to https://play.google.com/apps/publish/?account=8012316618525581861#ManageReleasesPlace:p=org.sefaria.sefaria
