ca4b4aaaa943b26125ca96363061e4b3cd408e69
Yishai <yishai@sefaria.org>
2022-10-20 18:15:57 +0300

    feat: bdb on long click

--- LexiconBox.js
+++ LexiconBox.js
@@ -123,18 +123,37 @@ const LexiconText = ({ value, handleOpenURL, lang, fSize, style }) => {
   );
 }
 
-const makeSenseTree = content => {
-  const grammar     = ('grammar' in content) ? '('+ content['grammar']['verbal_stem'] + ')' : "";
-  const def         = ('definition' in content) ? (content['definition']) : "";
-  const alternative = ('alternative' in content) ? (content['alternative']) : "";
-  const notes       = ('notes' in content) ? (content['notes']) : "";
+const makeSenseTree = (content, isBDB) => {
+  if (!isBDB) {
+    const grammar = ('grammar' in content) ? '(' + content['grammar']['verbal_stem'] + ')' : "";
+    const def = ('definition' in content) ? (content['definition']) : "";
+    const alternative = ('alternative' in content) ? (content['alternative']) : "";
+    const notes = ('notes' in content) ? (content['notes']) : "";
 
-  const senses = ('senses' in content) ? content['senses'].reduce((accum, sense) => accum.concat(makeSenseTree(sense)), []) : null;
-  const firstItem = (grammar || def || alternative || notes) ? (`${grammar} ${def} ${alternative} ${notes}`) : null;
-  return [
-    firstItem,
-    senses,
-  ].filter(x => !!x);
+    const senses = ('senses' in content) ? content['senses'].reduce((accum, sense) => accum.concat(makeSenseTree(sense, isBDB)), []) : null;
+    const firstItem = (grammar || def || alternative || notes) ? (`${grammar} ${def} ${alternative} ${notes}`) : null;
+    return [
+      firstItem,
+      senses,
+    ].filter(x => !!x);
+  } else {
+    const note = ('note' in content) ? '<em>Note.</em>' : '';
+    const preNun = ('pre_num' in content) ? content['pre_num'] : '';
+    const allCited = ('all_cited' in content) ? '†' : '';
+    const num = ('num' in content) ? content['num'] : '';
+    const form = ('form' in content) ? content['form'] : '';
+    const occurrences = ('occurrences' in content) ? content['occurrences'] : '';
+    let def = ('definition' in content) ? `<span>${content['definition']}</span>` : '';
+    let senses = ('senses' in content) ? content['senses'].reduce((accum, sense) => accum.concat(makeSenseTree(sense, isBDB)), []) : null;
+    const pre = `${note} ${preNun} ${allCited}<b>${num}${form}</b><small>${occurrences}</small>`;
+    if (senses) {
+      senses[0] = `${pre} ${senses[0]}`;
+      return senses;
+    } else {
+      def = `${pre} ${def}`;
+      return [def];
+    }
+  }
 }
 
 const LexiconAttribution = ({ entry, handleOpenURL }) => {
@@ -170,11 +189,41 @@ const LexiconEntry = ({ entry, handleOpenURL }) => {
   const {themeStr, fontSize} = useContext(GlobalStateContext);
   const theme = getTheme(themeStr);
   const englishFontSize = 0.8*fontSize;  // we actually use 0.8x of font size when displaying text in textcolumn. we want to duplicate that size here
-  let headwords = [entry['headword']];
-  if ('alt_headwords' in entry) {
-    headwords = headwords.concat(entry['alt_headwords']);
+  const isBDB = (entry) => {
+    return RegExp(/^BDB.*?Dict/).test(entry['parent_lexicon']);
   }
-  const headwordText = headwords.join(', ');
+  const headwordSting = (entry) => {
+    if (!isBDB(entry)) {
+      let headwords = [entry['headword']];
+      if ('alt_headwords' in entry) {
+        headwords = headwords.concat(entry['alt_headwords']);
+      }
+      return headwords.join(', ');
+    } else {
+      const peculiar = ('peculiar' in entry) ? '‡ ' : '';
+      const allCited = ('all_cited' in entry) ? '† ' : '';
+      const ordinal = ('ordinal' in entry) ? `${entry["ordinal"]} ` : '';
+      const hw = `<span dir="rtl">${entry['headword'].replace(/[⁰¹²³⁴⁵⁶⁷⁸⁹]*$/, '')}</span>`;
+      const occurrences = ('occurrences' in entry) ? `<small>${entry['occurrences']}</small>` : '';
+      const alts = ('alt_headwords' in entry) ? entry['alt_headwords']
+        .map(alt => {
+            var ahw = `<span dir="rtl">${alt['word']}</span>`;
+            var aocc = ('occurrences' in alt) ? `<small>${alt['occurrences']}</small>` : '';
+          return `<span>, ${ahw}${aocc}</span>`
+        })
+        .reduce((prev, curr) => [prev, curr]) : '';
+      const allHeadwords = ('headword_suffix' in entry) ? `<span>[${hw}${entry['headword_suffix']}]${occurrences}</span>` :
+        (entry['brackets'] == 'all') ? `<span>[${hw}${occurrences}${alts}]</span>` :
+          (entry['brackets'] == 'first_word') ? `<span>[${hw}${occurrences}]${alts}</span>` :
+            hw + occurrences + alts;
+      return (<SimpleHTMLView
+          text={peculiar + allCited + ordinal + allHeadwords}
+          lang="english"
+          extraStyles={[styles.he, {fontSize: fontSize}, theme.text]}
+      />);
+      }
+  }
+  const headwordText = headwordSting(entry)
   const morphology = ('morphology' in entry['content']) ?  (
     <LexiconText
       lang="english"
@@ -199,10 +248,9 @@ const LexiconEntry = ({ entry, handleOpenURL }) => {
       {langText}
     </View>
   );
-
   const endnotes = ('notes' in entry) ? <LexiconText lang='english' handleOpenURL={handleOpenURL} fSize={englishFontSize} value={entry['notes']}/> : null;
   const derivatives = ('derivatives' in entry) ? <LexiconText lang='english' handleOpenURL={handleOpenURL} fSize={englishFontSize} value={entry['derivatives']} /> : null;
-  const senses = makeSenseTree(entry['content']);
+  const senses = makeSenseTree(entry['content'], isBDB(entry));
   return (
     <View style={{marginTop: 20}}>
       <View>{entryHead}</View>
@@ -210,7 +258,7 @@ const LexiconEntry = ({ entry, handleOpenURL }) => {
         items={senses}
         renderItem={(item, index) => (
           <View key={index} style={{flexDirection: 'row'}}>
-            <SText lang='english' style={[styles.en, {textAlign: 'left', fontSize: englishFontSize}, theme.text]} lineMultiplier={1.15}>{`${index+1}. `}</SText>
+            <SText lang='english' style={[styles.en, {textAlign: 'left', fontSize: englishFontSize}, theme.text]} lineMultiplier={1.15}>{!isBDB(entry) && `${index+1}. `}</SText>
             <LexiconText
               lang='english'
               handleOpenURL={handleOpenURL}
