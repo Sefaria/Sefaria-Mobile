a29a352fbce766ba5650b98895e7278ee1954996
nsantacruz <noahssantacruz@gmail.com>
2023-01-18 12:57:15 +0200

    test(history): add TabHistory test

--- __tests__/PageHistory.test.js
+++ __tests__/PageHistory.test.js
@@ -1,11 +1,18 @@
-import { TabMetadata } from "../PageHistory";
+import { TabMetadata, TabHistory } from "../PageHistory";
 
 describe('TabMetadata', () => {
    test('initial tab correct', () => {
-     expect(TabMetadata.initialTabName()).toBe('Texts');
+       expect(TabMetadata.initialTabName()).toBe('Texts');
    });
 
    test('names function works', () => {
-     expect(TabMetadata.names()).toBe(TabMetadata._names);
-   })
+       expect(TabMetadata.names()).toBe(TabMetadata._names);
+   });
+});
+
+describe('TabHistory', () => {
+    test('historyByTab has right keys', () => {
+        const hist = new TabHistory();
+        expect(Object.keys(hist._historyByTab)).toStrictEqual(TabMetadata._names);
+    });
 });
