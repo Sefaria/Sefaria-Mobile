cac3381af705a8646ccea2d3ee57cfd353978272
nsantacruz <noahssantacruz@gmail.com>
2023-01-18 15:49:17 +0200

    test(history): add PageHistory test

--- __tests__/PageHistory.test.js
+++ __tests__/PageHistory.test.js
@@ -1,4 +1,4 @@
-import { TabMetadata, TabHistory } from "../PageHistory";
+import {TabMetadata, TabHistory, PageHistory} from "../PageHistory";
 
 describe('TabMetadata', () => {
    test('initial tab correct', () => {
@@ -14,5 +14,20 @@ describe('TabHistory', () => {
     test('historyByTab has right keys', () => {
         const hist = new TabHistory();
         expect(Object.keys(hist._historyByTab)).toStrictEqual(TabMetadata._names);
+    });
+});
+
+describe('PageHistory', () => {
+    const statesToPushTable = [
+        [{'sup': 1}],
+        [{'hello': 1}, {'hello': 2}, {'sup': 3}]
+    ]
+    test.each(statesToPushTable)('push state', (...states) => {
+        const hist = new PageHistory();
+        states.map(state => hist.forward({ state }));
+        [...states].reverse().map((state, i) => {
+            const prevState = hist.back();
+            expect(prevState).toStrictEqual(states[states.length-i-1]);
+        })
     });
 });
