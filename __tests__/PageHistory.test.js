8459765722e327181de52e40ee292fdc34a87609
nsantacruz <noahssantacruz@gmail.com>
2023-01-18 12:53:26 +0200

    test(history): add another test

--- __tests__/PageHistory.test.js
+++ __tests__/PageHistory.test.js
@@ -4,4 +4,8 @@ describe('TabMetadata', () => {
    test('initial tab correct', () => {
      expect(TabMetadata.initialTabName()).toBe('Texts');
    });
+
+   test('names function works', () => {
+     expect(TabMetadata.names()).toBe(TabMetadata._names);
+   })
 });
