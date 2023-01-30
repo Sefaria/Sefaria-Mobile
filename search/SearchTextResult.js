b4749ebbf88d5ab360cde42a993773147dcc3d64
nsantacruz <noahssantacruz@gmail.com>
2023-01-30 14:10:01 +0200

    feat(search): add margins between results

--- search/SearchTextResult.js
+++ search/SearchTextResult.js
@@ -16,7 +16,7 @@ export const SearchTextResult = ({text, tref, heRef, openRef, lang, versionTitle
     const en = lang === "english" ? text : "";
     const he = lang === "hebrew" ? text : "";
     return (
-        <StoryFrame>
+        <StoryFrame extraStyles={[{marginVertical: 16}]}>
             <StoryTitleBlock en={tref} he={Sefaria.normHebrewRef(heRef)} onClick={() => openRef(tref)}/>
             <ColorBarBox tref={tref}>
                 <StoryBodyBlock en={en} he={he}/>
