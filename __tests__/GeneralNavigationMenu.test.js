b29bc66f60ded08d10516b876cd3e106790b3309
Ephraim <ephraim@sefaria.org>
2023-01-31 17:13:45 +0200

    test(nav-redesign): Add more testing

---
+++ __tests__/GeneralNavigationMenu.test.js
@@ -0,0 +1,28 @@
+import {MenuItemsMeta} from "../GeneralNavigationMenu";
+
+describe('MenuItemsMeta', () => {
+   test('logged in menu items', () => {
+       expect(MenuItemsMeta.getMenuItems(true)).map(x => x['title']).toStrictEqual(['profile', 'updates', 'settings', 'language', 'help', 'about', 'logout', 'donate']);
+   });
+
+   test('logged out menu items', () => {
+       expect(MenuItemsMeta.getMenuItems(false)).map(x => x['title']).toStrictEqual(['register', 'login', 'updates', 'settings', 'language', 'help', 'about', 'donate'])
+   });
+   
+   test('logged in menu icons', () => {
+       expect(MenuItemsMeta.getMenuItems(true)).map(x => x['icon']).toStrictEqual(['Texts']);
+   });
+
+   test('logged out menu icons', () => {
+       expect(MenuItemsMeta.getMenuItems(false)).map(x => x['icon']).toStrictEqual([])
+   });
+
+   /*test('namesWithIcons works', () => {
+       const namesWithIcons = TabMetadata.namesWithIcons();
+       namesWithIcons.map(({name, icon}, i) => {
+           expect(name).toBe(TabMetadata._tabData[i].name);
+           expect(icon).toBe(TabMetadata._tabData[i].icon);
+       })
+   })*/
+});
+
