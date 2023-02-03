bb73d2cdd7609996d67388a79c6cdad075906c40
nsantacruz <noahssantacruz@gmail.com>
2023-02-03 15:32:27 +0200

    test(search): test getQuery

--- search/__tests__/FilterSearcher.test.js
+++ search/__tests__/FilterSearcher.test.js
@@ -1,4 +1,4 @@
-import {FilterSearcher} from "../FilterSearcher";
+import {FilterSearcher as FS} from "../FilterSearcher";
 import {FilterNode} from "@sefaria/search";
 import Sefaria from "../../sefaria";
 
@@ -18,7 +18,31 @@ describe('normalizeQuery', () => {
         expect(actualMatch[0]).toBe(query);
     });
     test.each(queries)('normalizeQuery', ({ orig, normal }) => {
-        const actualNormal = FilterSearcher._normalizeQuery(orig);
+        const actualNormal = FS._normalizeQuery(orig);
         expect(actualNormal).toBe(normal);
+    });
+});
+
+describe('getRegex', () => {
+    const queries = [
+        {query: "Yoma", toMatch: "Yoma"},
+        {query: "yoma", toMatch: "Yoma"},
+        {query: "yoma", toMatch: "Mishnah Yoma"},
+        {query: "yoma!", toMatch: "Mishnah Yoma"},
+        {query: "yoma-", toNotMatch: "Mishnah Yoma"},
+        {query: "yom", toMatch: "Mishnah Yoma"},
+        {query: "יומא", toMatch: "משנה יומא"},
+    ];
+    test.each(queries)('getRegex', ({ query, toMatch, toNotMatch }) => {
+        query = FS._normalizeQuery(query);
+        const regex = FS._getRegex(query);
+        const expectedResult = !!toMatch;
+        expect(regex.test(toMatch || toNotMatch)).toBe(expectedResult);
+    });
+    test.each(queries)('queryMatchesText', ({ query, toMatch, toNotMatch }) => {
+        query = FS._normalizeQuery(query);
+        const testResult = FS._queryMatchesText(query, toMatch || toNotMatch);
+        const expectedResult = !!toMatch;
+        expect(testResult).toBe(expectedResult);
     });
 });
