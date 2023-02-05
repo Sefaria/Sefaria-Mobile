8bb9e3fdc39726669ce79c04562a469f4a1e8a76
nsantacruz <noahssantacruz@gmail.com>
2023-02-05 14:35:45 +0200

    feat(text nav): basic learning schedules box

--- TextsPage.js
+++ TextsPage.js
@@ -1,7 +1,9 @@
-import React, {useCallback} from 'react';
+import React, {useCallback, useState} from 'react';
 import {View, FlatList, Text} from "react-native";
 import Sefaria from "./sefaria";
-import {CategoryButton, CategoryColorLine} from "./Misc";
+import {CategoryButton, CategoryColorLine, FlexFrame, GreyBoxFrame, LoadingView, SefariaPressable} from "./Misc";
+import {useAsyncVariable, useGlobalState} from "./Hooks";
+import styles from "./Styles";
 
 const useCategoryButtonProps = (tocItem, setCategories) => {
     const {category, heCategory, enDesc, heDesc, enShortDesc, heShortDesc} = tocItem;
@@ -20,6 +22,67 @@ const useCategoryButtonProps = (tocItem, setCategories) => {
         title, description, onPress,
     }
 }
+
+const TopLevelCategoryOrLearningSchedulesBox = ({item, setCategories, openRef}) => (
+    item.isSplice ?
+        (<LearningSchedulesBox openRef={openRef} />) :
+        (<TopLevelCategory item={item} setCategories={setCategories}/>)
+);
+
+const useCalendarItems = () => {
+    const {interfaceLanguage, preferredCustom} = useGlobalState();
+    const calendarLoaded = useAsyncVariable(!!Sefaria.calendar, Sefaria._loadCalendar);
+    const [galusOrIsrael, setGalusOrIsrael] = useState(Sefaria.getDefaultGalusStatus(interfaceLanguage));
+    useAsyncVariable(!!Sefaria.galusOrIsrael, Sefaria.getGalusStatus, setGalusOrIsrael, Sefaria.galusOrIsrael);
+    const calendarItems = Sefaria.getCalendars(preferredCustom, galusOrIsrael === 'diaspora');
+    return {
+        calendarLoaded,
+        calendarItems,
+    };
+}
+
+const getCalendarItemsForTextsPage = calendarItems => {
+    const textsPageCalendarTitles = new Set(['Parashat Hashavua', 'Haftarah (S)', 'Haftarah (A)','Daf Yomi']);
+    return calendarItems.filter(item => textsPageCalendarTitles.has(item.title.en));
+};
+
+const LearningSchedulesBox = ({item, openRef}) => {
+    const { calendarLoaded, calendarItems } = useCalendarItems();
+    const textsPageCalendarItems = getCalendarItemsForTextsPage(calendarItems);
+    return (
+        <GreyBoxFrame>
+            <Text>{"Learning Schedules"}</Text>
+            { calendarLoaded ?
+                <LearningScheduleTable calendarItems={textsPageCalendarItems} openRef={openRef} />
+                :
+                <LoadingView />
+            }
+        </GreyBoxFrame>
+    );
+};
+
+const LearningScheduleTable = ({ calendarItems, openRef }) => {
+    return (
+        <FlexFrame dir={"column"}>
+            {calendarItems.map(item => (
+                <LearningScheduleRow calendarItem={item} openRef={openRef} />
+            ))}
+        </FlexFrame>
+    );
+};
+
+const LearningScheduleRow = ({ calendarItem, openRef }) => {
+    const onPress = () => openRef(calendarItem.refs[0]);
+    return (
+        <SefariaPressable onPress={onPress}>
+            <FlexFrame dir={"row"} justifyContent={"space-between"}>
+                <Text style={[styles.flex1]}>{calendarItem.title.en}</Text>
+                <Text style={[styles.flex1]}>{calendarItem.refs[0]}</Text>
+            </FlexFrame>
+        </SefariaPressable>
+    );
+};
+
 const TopLevelCategory = ({item: tocItem, setCategories}) => {
     const categoryButtonProps = useCategoryButtonProps(tocItem, setCategories);
     return (
@@ -38,14 +101,24 @@ const TextsPageHeader = () => {
     );
 };
 
-export const TextsPage = ({setCategories}) => {
+export const TextsPage = ({setCategories, openRef}) => {
+    const { theme } = useGlobalState();
     const tocItems = Sefaria.tocItemsByCategories([]);
+    const data = [...tocItems];
+    data.splice(3, 0, {isSplice: true});
     return (
         <FlatList
-            data={tocItems}
-            contentContainerStyle={{paddingHorizontal: 15}}
+            data={data}
+            contentContainerStyle={[{paddingHorizontal: 15}, theme.mainTextPanel]}
             ListHeaderComponent={TextsPageHeader}
-            renderItem={props => <TopLevelCategory {...props} setCategories={setCategories}/>}
+            keyExtractor={item => item.isSplice ? 'splice' : item.category}
+            renderItem={props => (
+                <TopLevelCategoryOrLearningSchedulesBox
+                    {...props}
+                    setCategories={setCategories}
+                    openRef={openRef}
+                />
+            )}
         />
     );
 };
