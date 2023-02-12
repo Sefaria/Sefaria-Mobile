699bfd448b6378a6f5c09384a5fbdcd67f4d8444
Ephraim <ephraim@sefaria.org>
2023-02-12 09:14:52 +0200

    refactor(nav-redesign): rename some components

--- AccountNavigationMenu.js
+++ AccountNavigationMenu.js
@@ -66,17 +66,17 @@ const AccountNavigationMenuButton = ({titleKey, icon, callbackFunc, textStyles, 
   );
 }; 
 
-const BlueBackgroundAccountNavigationMenuButton = ({...menuButtonProps}) => {
+const SefariaBackgroundAccountNavigationMenuButton = ({...menuButtonProps}) => {
     const {  theme } = useGlobalState();
     return(<AccountNavigationMenuButton {...menuButtonProps} textStyles={[theme.sefariaColorButtonText]} containerStyles={[theme.sefariaColorButton]}/>);
 };
 
-const BlueTextAccountNavigationMenuButton = ({...menuButtonProps}) => {
+const SefariaTextAccountNavigationMenuButton = ({...menuButtonProps}) => {
     const {  theme } = useGlobalState();
     return(<AccountNavigationMenuButton {...menuButtonProps} textStyles={[theme.sefariaColorText]}/> );
 };
 
-const GrayTextAccountNavigationMenuButton = ({...menuButtonProps}) => {
+const SecondaryTextAccountNavigationMenuButton = ({...menuButtonProps}) => {
     const {  theme } = useGlobalState();
     return(<AccountNavigationMenuButton {...menuButtonProps} textStyles={[theme.secondaryText]}/> );
 };
@@ -111,15 +111,15 @@ const useMenuButtonObjects = () => {
 export class MenuItemsMeta{
   static _items = [
     {title: 'profile', icon: 'profile-nav', loggedIn: true, ButtonComponent: AccountNavigationMenuButton, actionProps:{action: "menu", destination:"profile"}},
-    {title: 'signup', icon: 'profile-nav', loggedIn: false, ButtonComponent: BlueTextAccountNavigationMenuButton, actionProps:{action: "menu", pushHistory: false,  destination:"register"}} , 
+    {title: 'signup', icon: 'profile-nav', loggedIn: false, ButtonComponent: SefariaTextAccountNavigationMenuButton, actionProps:{action: "menu", pushHistory: false,  destination:"register"}} , 
     {title: 'login', icon: 'login', loggedIn: false, ButtonComponent: AccountNavigationMenuButton, actionProps:{action: "menu",  pushHistory: false, destination:"login"}},
     {title: 'updates', icon: 'bell', ButtonComponent: AccountNavigationMenuButton, actionProps:{action: "menu", destination:"updates"}}, 
     {title: 'settings', icon: 'settings', ButtonComponent: AccountNavigationMenuButton, actionProps:{action: "menu", destination:"settings"}}, 
     {title: 'interfaceLanguage', icon: 'globe', ButtonComponent: InterfaceLanguageMenuButton, actionProps: {}}, 
     {title: 'help', icon: 'help', ButtonComponent: AccountNavigationMenuButton, actionProps:{action: "uri", destination:"https://www.sefaria.org/help"}}, 
     {title: 'aboutSefaria', icon: 'about', ButtonComponent: AccountNavigationMenuButton, actionProps:{action: "uri", destination:"https://www.sefaria.org/about"}}, 
-    {title: 'logout', icon: 'logout', loggedIn: true, ButtonComponent: GrayTextAccountNavigationMenuButton, actionProps:{action: "logout"}},
-    {title: 'donate', icon: 'heart-white', ButtonComponent: BlueBackgroundAccountNavigationMenuButton, actionProps:{action: "uri", destination:"https://www.sefaria.org/donate/mobile"}},
+    {title: 'logout', icon: 'logout', loggedIn: true, ButtonComponent: SecondaryTextAccountNavigationMenuButton, actionProps:{action: "logout"}},
+    {title: 'donate', icon: 'heart-white', ButtonComponent: SefariaBackgroundAccountNavigationMenuButton, actionProps:{action: "uri", destination:"https://www.sefaria.org/donate/mobile"}},
   ]
   static getMenuItems(isLoggedIn){
     const filterFunc = (x) => typeof x.loggedIn == 'undefined' || x.loggedIn === isLoggedIn;
