19f2b65ba283e506a059fb80cf08807190f2b11f
nsantacruz <noahssantacruz@gmail.com>
2023-02-14 20:53:40 +0200

    feat(texts nav): skeleton of text category page

---
+++ TextCategoryPage.js
@@ -0,0 +1,21 @@
+import React from "react";
+import {SectionList, View} from "react-native";
+
+/*
+add cat if at Talmud or Tosefta
+get aboutCat for description (again, different for Talmud and Tosefta)
+update category, catTitle and heCatTitle for Talmud, Tosefta and Commentaries
+Sefaria.tocObjectByCategories
+Sefaria.tocItemsByCategories
+
+calculate tree of contents
+render
+
+ */
+export const TextCategoryPage = () => {
+  return (
+      <SectionList
+          sections={}
+      />
+  );
+};
