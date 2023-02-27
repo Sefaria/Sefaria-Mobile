1801aff8838e59eba65879db16084c31344ac453
nsantacruz <noahssantacruz@gmail.com>
2023-02-27 15:45:46 +0200

    feat: add useIsKeyboardVisible hook

--- Hooks.js
+++ Hooks.js
@@ -197,3 +197,28 @@ export const useGetUserSettingsObj = () => {
     }
 }
 
+export const useIsKeyboardVisible = () => {
+    const [isKeyboardVisible, setKeyboardVisible] = useState(false);
+
+    useEffect(() => {
+        const keyboardDidShowListener = Keyboard.addListener(
+            'keyboardDidShow',
+            () => {
+                setKeyboardVisible(true); // or some other action
+            }
+        );
+        const keyboardDidHideListener = Keyboard.addListener(
+            'keyboardDidHide',
+            () => {
+                setKeyboardVisible(false); // or some other action
+            }
+        );
+
+        return () => {
+            keyboardDidHideListener.remove();
+            keyboardDidShowListener.remove();
+        };
+    }, []);
+
+    return isKeyboardVisible;
+}
