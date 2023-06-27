5390d71d8d5fa9dcbc9dece5e18cab6f0fc89db1
nsantacruz <noahssantacruz@gmail.com>
2023-06-27 11:51:39 +0300

    fix: fix typo

--- ios/ReaderApp/AppDelegate.mm
+++ ios/ReaderApp/AppDelegate.mm
@@ -48,7 +48,7 @@ static NSString *const kRNConcurrentRoot = @"concurrentRoot";
 #endif
 
   NSDictionary *initProps = [self prepareInitialProps];
-  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"RnDiffApp", initProps);
+  UIView *rootView = RCTAppSetupDefaultRootView(bridge, @"ReaderApp", initProps);
 
   if (@available(iOS 13.0, *)) {
     rootView.backgroundColor = [UIColor systemBackgroundColor];
