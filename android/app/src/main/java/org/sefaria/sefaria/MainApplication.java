6f828db666ac3c9c9307e8c88317b812856eb27c
nsantacruz <noahssantacruz@gmail.com>
2023-08-27 14:10:23 +0300

    chore: upgrade to RN 71

--- android/app/src/main/java/org/sefaria/sefaria/MainApplication.java
+++ android/app/src/main/java/org/sefaria/sefaria/MainApplication.java
@@ -1,7 +1,6 @@
 package org.sefaria.sefaria;
 
 import androidx.multidex.MultiDexApplication;
-import android.content.Context;
 
 import android.content.res.Configuration;
 import expo.modules.ApplicationLifecycleDispatcher;
@@ -11,15 +10,13 @@ import com.facebook.react.PackageList;
 import com.facebook.react.ReactApplication;
 import com.reactnativecommunity.webview.RNCWebViewPackage;
 import org.devio.rn.splashscreen.SplashScreenReactPackage;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
-import com.facebook.react.config.ReactFeatureFlags;
+import com.facebook.react.defaults.DefaultNewArchitectureEntryPoint;
+import com.facebook.react.defaults.DefaultReactNativeHost;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
-import org.sefaria.sefaria.newarchitecture.MainApplicationReactNativeHost;
 import com.facebook.react.modules.i18nmanager.I18nUtil;
-import java.lang.reflect.InvocationTargetException;
 
 import java.util.Arrays;
 import java.util.List;
@@ -28,7 +25,7 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
 
   private final ReactNativeHost mReactNativeHost =
 
-  new ReactNativeHostWrapper(this, new ReactNativeHost(this) {
+  new ReactNativeHostWrapper(this, new DefaultReactNativeHost(this) {
       @Override
       public boolean getUseDeveloperSupport() {
         return BuildConfig.DEBUG;
@@ -58,30 +55,34 @@ public class MainApplication extends MultiDexApplication implements ReactApplica
       protected String getJSMainModuleName() {
         return "index";
       }
+      @Override
+      protected boolean isNewArchEnabled() {
+        return BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
+      }
+      @Override
+      protected Boolean isHermesEnabled() {
+        return BuildConfig.IS_HERMES_ENABLED;
+      }
     });
 
-  private final ReactNativeHost mNewArchitectureNativeHost =
-      new MainApplicationReactNativeHost(this);
 
   @Override
   public ReactNativeHost getReactNativeHost() {
     ApplicationLifecycleDispatcher.onApplicationCreate(this);
-    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
-      return mNewArchitectureNativeHost;
-    } else {
-      return mReactNativeHost;
-    }
+    return mReactNativeHost;
   }
 
   @Override
   public void onCreate() {
     super.onCreate();
-    // If you opted-in for the New Architecture, we enable the TurboModule system
-    ReactFeatureFlags.useTurboModules = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED;
     I18nUtil sharedI18nUtilInstance = I18nUtil.getInstance();
     //sharedI18nUtilInstance.forceRTL(this,false);
     sharedI18nUtilInstance.allowRTL(this, false);
     SoLoader.init(this, /* native exopackage */ false);
+    if (BuildConfig.IS_NEW_ARCHITECTURE_ENABLED) {
+      // If you opted-in for the New Architecture, we load the native entry point for this app.
+      DefaultNewArchitectureEntryPoint.load();
+    }
   }
 
   @Override
