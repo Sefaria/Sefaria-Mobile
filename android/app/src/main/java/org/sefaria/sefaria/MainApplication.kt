5f3ccacf8c42a0ede869fe11e440fe8919d53860
nsantacruz <noahssantacruz@gmail.com>
2024-01-15 10:50:31 +0200

    fix: don't create anonymous subclass when you're not overriding methods! (aka, learn a bit of kotlin)

--- android/app/src/main/java/org/sefaria/sefaria/MainApplication.kt
+++ android/app/src/main/java/org/sefaria/sefaria/MainApplication.kt
@@ -16,8 +16,7 @@ import com.facebook.soloader.SoLoader
 
 class MainApplication : Application(), ReactApplication {
 
-  override val reactNativeHost: ReactNativeHost =
-      object : ReactNativeHostWrapper(this, object : DefaultReactNativeHost(this) {
+  override val reactNativeHost: ReactNativeHost = ReactNativeHostWrapper(this, object : DefaultReactNativeHost(this) {
         override fun getPackages(): List<ReactPackage> =
             PackageList(this).packages.apply {
               // Packages that cannot be autolinked yet can be added manually here, for example:
@@ -30,7 +29,7 @@ class MainApplication : Application(), ReactApplication {
 
         override val isNewArchEnabled: Boolean = BuildConfig.IS_NEW_ARCHITECTURE_ENABLED
         override val isHermesEnabled: Boolean = BuildConfig.IS_HERMES_ENABLED
-      }) {}
+      })
 
   override val reactHost: ReactHost
     get() = getDefaultReactHost(this.applicationContext, reactNativeHost)
