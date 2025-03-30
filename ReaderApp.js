8d3bf1b162958c1b2d6a8e399c56a787fdddca59
Daniel Schreiber <danielschreiber@sefaria.org>
2025-03-30 18:42:14 +0300

    In case of an unfound ref, retring the call with on up ref. So removing the last part of the ref and making a recursive call.

--- ReaderApp.js
+++ ReaderApp.js
@@ -739,6 +739,13 @@ class ReaderApp extends React.PureComponent {
         }).catch(error => {
           console.log(error);
           if (error == "Return to Nav") {
+              // Trying to get text one up recursively.
+              const refUpOne = Sefaria.refUpOne(ref, true);
+              if (ref !== refUpOne && numTries <= 4) { //Break if there is no more ref up to do. Number limit is just in case of a edge case
+                console.log(`Couldn't find ref. Removing last part of ref and trying again\nNew ref: ${refUpOne}. Old ref: ${ref}`)
+                this.loadNewText({ ref: refUpOne, versions, isLoadingVersion, numTries: numTries + 1 }).then(resolve);
+              }
+
             this.openTextTocDirectly(Sefaria.textTitleForRef(ref));
             resolve();
             return;
