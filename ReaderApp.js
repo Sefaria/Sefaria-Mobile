b8ed3080bb295c4947c738fde95163aa15bdd3b6
schreibersefaria <danielschreiber@sefaria.org>
2025-04-03 11:39:22 +0300

    fix typo

--- ReaderApp.js
+++ ReaderApp.js
@@ -740,7 +740,7 @@ class ReaderApp extends React.PureComponent {
           console.log(`Dealing with error: ${error}. Ref: ${ref}`);
           if (error == "Return to Nav") {
             // In case of unfound references, try going one ref up (up to the book) before dealing with error by returning to nav.
-            // We do this to avoid failing in case of references to a non existent ref after a changing index of in book.
+            // We do this to avoid failing in case of references to a non existent ref after a changing index of a book.
             const refUpOne = Sefaria.refUpOne(ref, true);
             // Break if there is no more ref up to do.
             // Number limit is just in case of a edge case.
