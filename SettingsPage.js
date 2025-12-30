7300e4d7bed5b9e1e13c788f2f540d932cf1b054
YishaiGlasner <yishai@sefaria.org>
2025-12-30 15:47:02 +0200

    feat(change host): make it slightly less ugly.

--- SettingsPage.js
+++ SettingsPage.js
@@ -208,10 +208,10 @@ const VersionNumberChangeHost = ({versionNumber}) => {
   }
   return <>
     <Text onPress={handlePress}> {versionNumber}</Text>
-    {showHostChange && <>
+    {showHostChange && <View style={{ flexDirection: 'row', gap: 8 }}>
         <TextInput placeholder="Set host url" onChangeText={setHost} value={host}/>
         <Button title="Save" onPress={handleSubmit} />
-    </>}
+    </View>}
   </>;
 }
 
