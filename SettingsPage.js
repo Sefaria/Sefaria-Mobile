a572996d7013530ed70269b358914f0a795ac5f8
YishaiGlasner <yishai@sefaria.org>
2026-06-11 17:46:08 +0300

    fix(SettingPage): keyboard won't cover the apihost box

--- SettingsPage.js
+++ SettingsPage.js
@@ -289,7 +289,7 @@ const SettingsPage = ({ close, logout, openUri, syncProfile }) => {
         <Text style={[langStyle, styles.settingsHeader, theme.text]}>{strings.settings.toUpperCase()}</Text>
       </View>
 
-      <ScrollView contentContainerStyle={styles.menuContent}>
+      <ScrollView contentContainerStyle={styles.menuContent} automaticallyAdjustKeyboardInsets={true}>
 
         <ButtonToggleSection
           langStyle={langStyle}
